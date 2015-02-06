c3ae2c2aacc370ae504eb1b21cde7ab21f594736
Dominic Evans <dominic.evans@uk.ibm.com>
2015-02-06 13:33:22 +0000

    replace copyright headers with apache 2.0 cla ones

--- mqlight-samples/src/main/java/com/ibm/mqlight/api/samples/Send.java
+++ mqlight-samples/src/main/java/com/ibm/mqlight/api/samples/Send.java
@@ -1,21 +1,21 @@
-/*
- * <copyright
- * notice="lm-source-program"
- * pids="5725-P60"
- * years="2015"
- * crc="3568777996" >
- * Licensed Materials - Property of IBM
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * 5725-P60
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * (C) Copyright IBM Corp. 2015
- *
- * US Government Users Restricted Rights - Use, duplication or
- * disclosure restricted by GSA ADP Schedule Contract with
- * IBM Corp.
- * </copyright>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package com.ibm.mqlight.api.samples;
 
 import java.io.ByteArrayOutputStream;
--- mqlight/src/main/java/com/ibm/mqlight/api/impl/engine/EngineConnection.java
+++ mqlight/src/main/java/com/ibm/mqlight/api/impl/engine/EngineConnection.java
@@ -1,24 +1,21 @@
-/*
- *   <copyright
- *   notice="oco-source"
- *   pids="5725-P60"
- *   years="2015"
- *   crc="1438874957" >
- *   IBM Confidential
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *   OCO Source Materials
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *   5724-H72
- *
- *   (C) Copyright IBM Corp. 2015
- *
- *   The source code for the program is not published
- *   or otherwise divested of its trade secrets,
- *   irrespective of what has been deposited with the
- *   U.S. Copyright Office.
- *   </copyright>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package com.ibm.mqlight.api.impl.engine;
 
 import java.util.HashMap;
